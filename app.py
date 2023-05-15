7ba390d297243750df08c1208c1ad61c257fcec3
sushskvnitn <sushantkogurwar@gmail.com>
2023-05-15 20:34:29 +0530

    added database connection and env for securing database key

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
 from flask import Flask , render_template, jsonify
+from database import load_jobs_from_databases
 app = Flask(__name__)
 #passing this jobs to the home.html using render_template
-JOBS =[
-    {
-        'id':1,
-        'title':'Software Engineer',
-        'description':'Python Developer',
-        'salary':'4000$'
-    },
-    {
-        'id':2,
-        'title':'Software Engineer',
-        'description':'app Developer',
-        'salary':'2040$'
-    },
-    {
-        'id':3,
-        'title':'Software Engineer',
-        'description':'web Developer',
-        'salary':'2400$'
-    },
-]
+# JOBS =[
+#     {
+#         'id':1,
+#         'title':'Software Engineer',
+#         'description':'Python Developer',
+#         'salary':'4000$'
+#     },
+#     {
+#         'id':2,
+#         'title':'Software Engineer',
+#         'description':'app Developer',
+#         'salary':'2040$'
+#     },
+#     {
+#         'id':3,
+#         'title':'Software Engineer',
+#         'description':'web Developer',
+#         'salary':'2400$'
+#     },
+# ]
+
+
 #html route to render the html page
 @app.route('/')
 def Hello():
+    jobs=load_jobs_from_databases()
     #returning the html page using render_template
-    return render_template('home.html', jobs=JOBS ,compony='SUSHANT' )
+    # return render_template('home.html', jobs=JOBS ,compony='SUSHANT' )
+    return render_template('home.html', jobs=jobs ,compony='SUSHANT' )
 
 #api route to render the json data
 @app.route('/api/jobs')
 def get_jobs():
     #returning the json data using jsonify
-    return jsonify(JOBS)
+    jobs= load_jobs_from_databases()
+    return jsonify(jobs)
 
 # writing below code to run the app
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine,text
+from dotenv import load_dotenv
+import os
+load_dotenv()
+Connection_database = os.getenv("Connection_database")
+engine = create_engine(
+   Connection_database,
+   connect_args={
+         "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+         }
+   }
+)
+def load_jobs_from_databases():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        Jobs = []
+        column_names = result.keys()  # Retrieve the column names
+
+        for row in result.fetchall():
+            row_dict = {column_name: row_value for column_name, row_value in zip(column_names, row)}
+            Jobs.append(row_dict)
+
+        return Jobs
+
+
